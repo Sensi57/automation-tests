099766ac6a41a848c0c5d17151731c3c0c582bcd
Sensi <averus2018@mail.ru>
2025-11-25 20:52:01 +0500

    UPD: testing

--- run_all_tests.py
+++ run_all_tests.py
@@ -1,13 +1,11 @@
 import pytest
 
-print("=== Running Chrome tests ===")
-pytest.main(["tests/test_chrome.py", "-v"])
-
-print("=== Running Firefox tests ===")
-pytest.main(["tests/test_firefox.py", "-v"])
-
-print("=== Running Safari tests ===")
-pytest.main(["tests/test_safari.py", "-v"])
-
-print("=== Running functional tests ===")
-pytest.main(["tests/test1.py", "tests/test2.py", "-v"])
+# Запуск всех тестов одновременно
+pytest.main([
+    "tests/test_chrome.py",
+    "tests/test_firefox.py",
+    "tests/test_safari.py",
+    "tests/test1.py",
+    "tests/test2.py",
+    "-v"
+])
